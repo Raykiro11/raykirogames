9e70c662ec768922c71c43f37474d6d3696b2337
Raykiro11 <leonardofornazari@gmail.com>
2025-09-16 14:22:30 +0200

    Update main.py

--- backend/game-review-api/src/main.py
+++ backend/game-review-api/src/main.py
@@ -26,7 +26,7 @@ RAWG_API_KEY = os.getenv('RAWG_API_KEY') or '5ff00f2791e447d0aee2156bc93c4b7e'
 RAWG_BASE_URL = 'https://api.rawg.io/api'
 
 # Inicializar extensões
-CORS(app, origins=["http://localhost:5173", "http://localhost:5002"])
+CORS(app, origins=['https://www.raykirogames.com/games', "http://localhost:5173", "http://localhost:5002"])
 jwt = JWTManager(app)
 limiter = Limiter(
     key_func=get_remote_address,
