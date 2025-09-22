3f440db53e61ef87e06bfaac92b12448c36a728f
Raykiro11 <leonardofornazari@gmail.com>
2025-09-22 13:31:46 +0200

    Update GameDetailPage.jsx

--- frontend/game-review-frontend/src/pages/GameDetailPage.jsx
+++ frontend/game-review-frontend/src/pages/GameDetailPage.jsx
@@ -13,7 +13,7 @@ function GameDetailPage() {
     const fetchGameDetails = async () => {
       try {
         setLoading(true)
-        const response = await fetch(`http://localhost:5000/api/games/${id}` )
+        const response = await fetch(`https://localhost:5000/api/games/${id}` )
         const data = await response.json()
         
         if (data.status === 'success') {
